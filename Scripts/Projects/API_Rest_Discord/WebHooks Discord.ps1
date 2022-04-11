d000b193baa511679892a300a82f1e6798add083
Jean-Baptiste <87148630+Jean-BaptisteC@users.noreply.github.com>
2022-04-11 22:59:04 +0200

    Add API REST Discord

---
+++ Scripts/Projects/API_Rest_Discord/WebHooks Discord.ps1
@@ -0,0 +1,18 @@
+﻿#Send request on Discord with content and username
+$webHookUrl = '{Your token Discord}'
+
+$username = 'Bot'
+
+$content = @'
+Example
+- Example 1
+- Example 2
+'@
+
+$payload = [PSCustomObject]@{
+
+    username = $username
+    content = $content
+}
+
+Invoke-RestMethod -Uri $webHookUrl -Method Post -Body ($payload | ConvertTo-Json -Depth 4) -ContentType 'Application/Json'
---
+++ Scripts/Projects/API_Rest_Discord/WebHooks_Advanced_Discord.ps1
@@ -0,0 +1,22 @@
+﻿#Send request on Discord with content, color, title and username
+$webHookUrl = '{Your token Discord}'
+
+[System.Collections.ArrayList]$embedArray = @()
+
+$embedObject = [PSCustomObject]@{
+    username = 'Bot'
+    color = '1'
+    title = 'Example'
+    description = ('
+    - Example 1
+    - Example 2')
+}
+
+$embedArray.Add($embedObject)|Out-Null
+
+$payload = [PSCustomObject]@{
+    embeds = $embedObject
+
+}
+
+Invoke-RestMethod -Uri $webHookUrl -Body ($payload | ConvertTo-Json -Depth 4) -Method Post -ContentType 'application/json'
