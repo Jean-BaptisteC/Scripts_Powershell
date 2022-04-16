5c69411dbb0376fed632fc2dbb9519e0c91e043a
Jean-Baptiste <87148630+Jean-BaptisteC@users.noreply.github.com>
2022-04-16 11:58:01 +0200

    Improve convert csv/txt script

--- Scripts/Projects/Import_ExportFile.ps1
+++ Scripts/Projects/Import_ExportFile.ps1
@@ -1,28 +1,23 @@
 ﻿#Import-ExportFile 1.2
 
+Set-StrictMode -Version Latest
+
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
 [Windows.Forms.Application]::EnableVisualStyles()
 
-$Dir = 'C:'
-
-$FormFile = New-Object System.Windows.Forms.OpenFileDialog
-$FormFile.CheckFileExists = $true
-$FormFile.CheckPathExists = $true
-$FormFile.Filter = 'Fichier (*.csv, *.txt)|*.csv;*.txt|CSV|*.csv|Fichier texte|*.txt'
-$FormFile.InitialDirectory = "C:\Users\$env:USERNAME\Desktop\"
-$FormFile.Multiselect = $false
-$FormFile.Title = 'Sélectionner un fichier à exporter'
-
-$SavePath = New-Object System.Windows.Forms.SaveFileDialog
-$SavePath.CheckPathExists = $true
-$SavePath.RestoreDirectory = $false
-
 $script:balloon = New-Object System.Windows.Forms.NotifyIcon
 Register-ObjectEvent -InputObject $script:balloon -EventName BalloonTipClicked -Action{Start-Process $script:Output} | Out-Null
 
 function Open-File {
+    $FormFile = New-Object System.Windows.Forms.OpenFileDialog
+    $FormFile.CheckFileExists = $true
+    $FormFile.CheckPathExists = $true
+    $FormFile.Filter = 'Fichier (*.csv, *.txt)|*.csv;*.txt|CSV|*.csv|Fichier texte|*.txt'
+    $FormFile.InitialDirectory = "C:\Users\$env:USERNAME\Desktop\"
+    $FormFile.Multiselect = $false
+    $FormFile.Title = 'Sélectionner un fichier à exporter'
     if($FormFile.ShowDialog() -eq [System.Windows.Forms.DialogResult]::OK)
     {
         $File = $FormFile.Filename
@@ -34,8 +29,24 @@ function Open-File {
     }
 }
 
+function Save-File {
+    $FormSave = New-Object System.Windows.Forms.SaveFileDialog
+    $FormSave.CheckPathExists = $true
+    $FormSave.RestoreDirectory = $false
+    $FormSave.InitialDirectory = $Directory
+    if($FormSave.ShowDialog() -eq [System.Windows.Forms.DialogResult]::OK)
+    {
+        $script:Output = $FormSave.FileName
+        Export-File
+    }
+    else
+    {
+        exit
+    }
+}
+
 function Get-Extension {
-    $Dir = [System.IO.Path]::GetDirectoryName($File)
+    $Directory = [System.IO.Path]::GetDirectoryName($File)
     if([System.IO.Path]::GetExtension($File) -eq '.txt')
     {
         $Nom = [System.IO.Path]::GetFileNameWithoutExtension($File) + '.csv'
@@ -46,31 +57,22 @@ function Get-Extension {
         $Nom = [System.IO.Path]::GetFileNameWithoutExtension($File) + '.txt'
         $Extension = 'csv'
     }
-    $Check = "$Dir\$Nom"
+    $Check = "$Directory\$Nom"
     if(Test-Path $Check)
     {
         if([System.Windows.Forms.MessageBox]::Show('Attention ce fichier existe déja voulez vous écraser son contenu.','Modification de fichier',[System.Windows.Forms.MessageBoxButtons]::YesNo,[System.Windows.Forms.MessageBoxIcon]::Warning) -eq [System.Windows.Forms.DialogResult]::Yes)
         {
-            $script:Output = New-Item -Path $Dir -Name $Nom -ItemType File -Force
+            $script:Output = New-Item -Path $Directory -Name $Nom -ItemType File -Force
             Export-File
             }
             else
             {
-                $SavePath.InitialDirectory = $Dir
-                if($SavePath.ShowDialog() -eq [System.Windows.Forms.DialogResult]::OK)
-                {
-                    $script:Output = $SavePath.FileName
-                    Export-File
-                }
-                else
-                {
-                    exit
-                }
+                Save-File
             }
     }
     else
     {
-        $script:Output = New-Item -Path $Dir -Name $Nom -ItemType File
+        $script:Output = New-Item -Path $Directory -Name $Nom -ItemType File
         Export-File
     }
 }
@@ -81,12 +83,12 @@ function Export-File{
     )
     if($extension -eq 'txt')
     {
-        $script:Output = New-Item -Path $Dir -Name $Nom -ItemType File -Force
+        $script:Output = New-Item -Path $Directory -Name $Nom -ItemType File -Force
         Save-Txt -File $File
     }
     else
     {
-        $script:Output = New-Item -Path $Dir -Name $Nom -ItemType File -Force
+        $script:Output = New-Item -Path $Directory -Name $Nom -ItemType File -Force
         Save-Csv -File $File
     }
 }
@@ -97,7 +99,7 @@ function Save-Txt {#Export txt
     )
     Import-Csv -Path $File -Encoding Default | Export-Csv -Path $script:Output -Encoding UTF8 -Delimiter ',' -NoTypeInformation
     (Get-Content -Path $script:Output -Raw).replace('"','') | Set-Content -Path $script:Output
-    $Dir = [System.IO.Path]::GetDirectoryName($script:Output)
+    $Directory = [System.IO.Path]::GetDirectoryName($script:Output)
     Show-Notification
 }
 
@@ -107,15 +109,15 @@ function Save-Csv {#Export csv
     )
     Import-Csv -Path $File -Encoding Default | Export-Csv -Path $script:Output -Encoding UTF8 -Delimiter ',' -NoTypeInformation
     (Get-Content -Path $script:Output -Raw).replace('"','') | Set-Content -Path $script:Output
-    $Dir = [System.IO.Path]::GetDirectoryName($script:Output)
+    $Directory = [System.IO.Path]::GetDirectoryName($script:Output)
     Show-Notification
 }
 
 function Show-Notification {
-    $balloon.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon("C:\Windows\explorer.exe")
+    $balloon.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon('C:\Windows\explorer.exe')
     $balloon.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Info
-    $balloon.BalloonTipText = "Cliquez ici pour ouvrir"
-    $balloon.BalloonTipTitle = "Fichier Exporté"
+    $balloon.BalloonTipText = 'Cliquez ici pour ouvrir'
+    $balloon.BalloonTipTitle = 'Fichier Exporté'
     $balloon.Visible = $true
     $balloon.ShowBalloonTip(5000)
 }
